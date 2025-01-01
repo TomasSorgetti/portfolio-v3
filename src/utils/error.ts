d258ff58e16146b2c8c20bd8be1900c7c38fc00f
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-01 13:49:08 -0300

    feat: api change

---
+++ src/utils/error.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export const createCustomError = (
+  message: string,
+  statusCode: number,
+  data: any
+) => {
+  const error = new Error(message);
+  error.response = {
+    status: statusCode,
+    ...(data && { data }),
+  };
+
+  return error;
+};
+
+export const handleErrorResponse = (error: any, defaultMessage: string) => {
+  const message = error.message || defaultMessage;
+  const status = error.response?.status || 500;
+
+  console.error(message, error);
+  return NextResponse.json({ error: message }, { status });
+};
