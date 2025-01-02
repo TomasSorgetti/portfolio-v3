0c5e3f11da79569df24dcb5d126fb0f2b003bed9
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-01 23:10:59 -0300

    fix: solve conflict in custom errors

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -7,17 +7,10 @@ interface CustomErrorResponse {
 
 export const createCustomError = (
   message: string,
-<<<<<<< HEAD
   statusCode?: number,
   data?: unknown
 ): Error & { response?: CustomErrorResponse } => {
   const error: Error & { response?: CustomErrorResponse } = new Error(message);
-=======
-  statusCode: number,
-  data: any | null
-) => {
-  const error = new Error(message);
->>>>>>> 50e26751329e18e63b5d87c62260bc5f1760d5af
   error.response = {
     status: statusCode,
     ...(data as object),
