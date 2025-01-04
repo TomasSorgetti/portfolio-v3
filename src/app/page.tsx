9166f041a9a2525e73fc649bfdc2e020275ccf23
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-04 14:33:39 -0300

    feat: grid skills removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Skills from "@/layouts/home/Skills";
 import Cta from "@/layouts/home/Cta";
 import ProjectSlider from "@/components/sliders/ProjectSlider";
 import { projectList } from "@/data/projects";
-// import GridSkills from "@/layouts/home/GridSkills";
 
 export default async function Home() {
   return (
@@ -16,7 +15,6 @@ export default async function Home() {
       <ProjectSlider list={projectList} />
       <About />
       <Skills />
-      {/* <GridSkills /> */}
       <Cta />
       <Contact />
     </main>
